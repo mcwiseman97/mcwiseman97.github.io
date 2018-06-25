a07fa5ad519c66683715167585cb7df99f7ef01d
mcwiseman97 <mcwiseman97@gmail.com>
2018-06-25 11:14:01 -0600

    add api key

--- weather/scripts/script.js
+++ weather/scripts/script.js
@@ -1,1 +1,21 @@
+//KEY WtDgQpcXPTTHmmrANqYHpTyBZmr5gxi2
+
+// TEST TO SEE IF JS WORKS
 console.log(5 + 6);
+
+function getCondition(){
+    if(){
+
+    }
+    if(){
+
+    }
+    if(){
+
+    }
+}
+
+function changeSummeryImage(){
+
+
+}
