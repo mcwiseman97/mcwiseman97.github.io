4aeb98f1f4d31ceb1b58c5e8854ce0a0dcb5c55b
mcwiseman97 <mcwiseman97@gmail.com>
2018-06-26 10:17:36 -0600

    script test

--- weather/scripts/script.js
+++ weather/scripts/script.js
@@ -3,19 +3,19 @@
 // TEST TO SEE IF JS WORKS
 console.log(5 + 6);
 
-function getCondition(){
-    if(){
+// function getCondition(){
+//     // if(){
 
-    }
-    if(){
+//     // }
+//     // if(){
 
-    }
-    if(){
+//     // }
+//     // if(){
 
-    }
-}
+//     // }
+// }
 
-function changeSummeryImage(){
+// function changeSummeryImage(){
 
 
-}
+// }
