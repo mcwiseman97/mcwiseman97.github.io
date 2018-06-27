cde9265b611d16ce2322b7c41c7d370254889961
mcwiseman97 <mcwiseman97@gmail.com>
2018-06-27 15:17:43 -0600

    added semicolons

--- weather/scripts/script.js
+++ weather/scripts/script.js
@@ -32,39 +32,39 @@ function windDial(direction){
     switch (direction){
         case "North":
         case "N":
-         dial.setAttribute("class", "n") //"n" is the CSS rule selector
+         dial.setAttribute("class", "n");
          break;
         case "NE":
         case "NNE":
         case "ENE":
-         dial.setAttribute("class", "ne")
+         dial.setAttribute("class", "ne");
          break;
         case "NW":
         case "NNW":
         case "WNW":
-         dial.setAttribute("class", "nw")
+         dial.setAttribute("class", "nw");
          break;
         case "South":
         case "S":
-         dial.setAttribute("class", "s")
+         dial.setAttribute("class", "s");
          break;
         case "SE":
         case "SSE":
         case "ESE":
-         dial.setAttribute("class", "se")
+         dial.setAttribute("class", "se");
          break;
         case "SW":
         case "SSW":
         case "WSW":
-         dial.setAttribute("class", "sw")
+         dial.setAttribute("class", "sw");
          break;
         case "East":
         case "E":
-         dial.setAttribute("class", "e")
+         dial.setAttribute("class", "e");
          break;
         case "West":
         case "W":
-         dial.setAttribute("class", "w")
+         dial.setAttribute("class", "w");
          break;
     }
 }
