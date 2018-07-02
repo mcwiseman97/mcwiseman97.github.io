80a702927b34c4e0c38dcfa7135efbbef72f9842
mcwiseman97 <mcwiseman97@gmail.com>
2018-07-02 08:21:08 -0600

    test JS

--- weather/scripts/script.js
+++ weather/scripts/script.js
@@ -166,4 +166,54 @@ function getWeather(locData) {
       getHourly(locData); // Send data to getHourly function
       })
      .catch(error => console.log('There was an error: ', error))
-  } // end getWeather function
+  } 
+
+
+// Get next 12 hours of forecast data from API
+function getHourly(locData) {
+  const API_KEY = 'Paste Your API Key Here';
+  const CITY_CODE = locData['key'];
+  const URL = "https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"+CITY_CODE+"?apikey="+API_KEY;
+  fetch(URL)
+    .then(response => response.json())
+    .then(function (data) {
+    console.log('Json object from getHourly function:');
+    console.log(data); // See what we got back
+    // Get the first hour in the returned data
+    let date_obj = new Date(data[0].DateTime);
+    let nextHour = date_obj.getHours(); // returns 0 to 23
+    // Store into the object
+    locData["nextHour"] = nextHour;
+    // Counter for the forecast hourly temps
+    var i = 1;
+    // Get the temps for the next 12 hours
+    data.forEach(function (element) {
+      let temp = element.Temperature.Value;
+      let hour = 'hourTemp' + i;
+      locData[hour] = temp; // Store hour and temp to object
+      // New hiTemp variable, assign value from previous 12 hours
+      let hiTemp = locData.pastHigh;
+      // New lowTemp variable, assign value from previous 12 hours
+      let lowTemp = locData.pastLow;
+      // Check current forecast temp to see if it is 
+      // higher or lower than previous hi or low
+      if(temp > hiTemp){
+        hiTemp = temp;
+      } else if (temp < lowTemp){
+        lowTemp = temp;
+      }
+      // Replace stored low hi and low temps if they changed
+      if(hiTemp != locData.pastHigh){
+        locData["pastHigh"] = hiTemp; // When done, this is today's high temp
+      }
+      if(lowTemp != locData.pastLow){
+        locData["pastLow"] = lowTemp; // When done, this is today's low temp
+      }
+      i++; // Increase the counter by 1
+    }); // ends the foreach method
+    console.log('Finished locData object and data:');
+    console.log(locData);
+    buildPage(locData); // Send data to buildPage function
+    })
+    .catch(error => console.log('There was an error: ', error))
+} // end getHourly function
